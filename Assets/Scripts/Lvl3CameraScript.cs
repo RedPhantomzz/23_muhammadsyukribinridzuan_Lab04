5db1cd7f43c9a33896fe711cb71dba847047dafb
VerifyingUsername <lkyben6@gmail.com>
2022-01-24 15:45:01 +0800

    Camera follows player (Done by Ben)

---
+++ Assets/Scripts/Lvl3CameraScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lvl3CameraScript : MonoBehaviour
+{
+    public Vector3 Offset;
+    public Transform player;
+    public float cameraSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 finalPosition = player.position + Offset;
+        transform.position = Vector3.Lerp(transform.position, finalPosition, cameraSpeed * Time.deltaTime);
+    }
+}
